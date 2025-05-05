e39e89a5be256688b22e75c1c6a7745996544dc3
KUN <leeguandon@gmail.com>
2025-05-05 17:42:54 +0800

    update

---
+++ tools/llamafactory_0.9.3/llama_factory_0.9.3_qwen3_infer.sh
@@ -0,0 +1,2 @@
+export CUDA_VISIBLE_DEVICES=3,4,5,6
+llamafactory-cli chat "/home/gdli7/SparkLLM/tools/llamafactory_0.9.3/inference/qwen3_lora_sft.yaml"
---
+++ tools/llamafactory_0.9.3/llama_factory_0.9.3_qwen3_run_sft.sh
@@ -0,0 +1,2 @@
+export CUDA_VISIBLE_DEVICES=3,4,5,6
+llamafactory-cli train "/home/gdli7/SparkLLM/tools/llamafactory_0.9.3/train_lora/qwen3_lora_sft.yaml"
